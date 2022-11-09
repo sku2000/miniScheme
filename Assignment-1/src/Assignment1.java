a9f52cf5039a380a0d6e529207506773f7fb472e
stu2000 <1907109855@qq.com>
2022-11-09 15:55:53 +0800

    实现 Parser 和 Verifier

--- Assignment-1/src/Assignment1.java
+++ Assignment-1/src/Assignment1.java
@@ -1,12 +1,12 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 interface ASTVisitor {
     public <T> T forVar(Var v);
-    public <T> T forEmpty(Empty empty);
     public <T> T forSBool(SBool sBool);
     public <T> T forSInt(SInt sInt);
     public <T> T forAList(AList aList);
@@ -26,6 +26,11 @@ class Program implements AST {
     public <T> T accept(ASTVisitor ask) {
         return ask.forProgram(this);
     }
+
+    @Override
+    public String toString() {
+        return program.toString();
+    }
 }
 class Var implements AST {
     String v;
@@ -36,122 +41,131 @@ class Var implements AST {
     public <T> T accept(ASTVisitor ask) {
         return ask.forVar(this);
     }
+
+    @Override
+    public String toString() {
+        return v;
+    }
 }
 
 class SBool implements AST {
     int b;
     SBool(int b) {
-        b = b;
+        this.b = b;
     }
     @Override
     public <T> T accept(ASTVisitor ask) {
         return ask.forSBool(this);
     }
+
+    @Override
+    public String toString() {
+        if (b == 0)
+            return "#f";
+        else
+            return "#t";
+    }
 }
 
 class SInt implements AST {
-    int i;
-    SInt(int i) {
-        i = i;
+    long i;
+    SInt(long i) {
+        this.i = i;
     }
     @Override
     public <T> T accept(ASTVisitor ask) {
         return ask.forSInt(this);
     }
-}
 
-class Empty implements AST {
     @Override
-    public <T> T accept(ASTVisitor ask) {
-        return ask.forEmpty(this);
+    public String toString() {
+        return String.valueOf(i);
     }
 }
 
 class AList implements AST {
     ArrayList<AST> list;
-    AList(String text, ArrayList<AST> list) {
+    AList(ArrayList<AST> list) {
         this.list = list;
     }
     @Override
     public <T> T accept(ASTVisitor ask) {
         return ask.forAList(this);
     }
+
+    @Override
+    public String toString() {
+        list.toString();
+        Iterator<AST> it = list.iterator();
+        if (!it.hasNext())
+            return "()";
+
+        String str = "(";
+        for (;;) {
+            AST ast = it.next();
+            str += ast.toString();
+
+            if (!it.hasNext())
+                return str + ")";
+            str += " ";
+        }
+    }
 }
 
 class Parser {
-//    char[] chs;
-//    int opinter;
-//    AST parseList() {
-//        if (chs[opinter] == '(') {
-//            opinter++;
-//            AST ast = parseList();
-//            if (chs[opinter] != ')')
-//                throw new RuntimeException("不是合法的 list");
-//            return ast;
-//        } else {
-//            if (chs[opinter] == ')')
-//                return new Empty();
-//            else if (chs[opinter] == '\s' )
-//        }
-//    }
-//    void parseBlank() {
-//        char[] blanks = {'\s', '\t', '\n', '\r'};
-//
-//        for (char c : ) {
-//
-//        }
-//    }
-//    AST parseGrammar() {
-//        parseBlank();
-//        if (chs[opinter] == '(') {
-//            opinter++;
-//            return parseList();
-//        }
-//    }
     String text;
     AST results;
     Parser parserSymbols(String pattern, Function<String, AST> f) {
-        if (text.startsWith(pattern)) {
-            Pattern p = Pattern.compile(pattern);
-            Matcher m = p.matcher(text);
-            m.find();
-
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(text);
+        if (m.find()) {
             results = f.apply(m.group());
             text = m.replaceFirst("");
             return this;
         } else {
-            Parser p = new Parser();
-            p.text = "";
-            p.results = null;
-            return p;
+            Parser parser = new Parser();
+            parser.text = "";
+            parser.results = null;
+            return parser;
         }
     }
-    Parser parseBlank() {
-        // limt = 2 表示匹配1次，如果匹配成功，strs 包含两个元素，第二个元素是需要的元素
-        // 如何匹配失败，strs 则只包含一个元素
-        String[] strs = text.split("^[\\s\\t\\n\\r]+", 2);
-        if (strs.length == 2)
-            text = strs[2];
+
+    Parser parseSubList(Function<AST, AList> f) {
+        if (this.text.isEmpty() && results == null)
+            return this;
+        text = text.trim();
+        Parser parser = parseSub();
+
+        if (parser.text.isEmpty() && parser.results == null) {
+            results = f.apply(null);
+            return this;
+        }
+
+        AList aList = f.apply(parser.results);
+
+        text = text.trim();
+
+        parseSubList((res) -> {
+            if (res == null)
+                return aList;
+            aList.list.add(res);
+            return aList;
+        });
         return this;
     }
 
-    Parser ParseSubList() {
-        if (text == null && results == null)
-            return this;
-
-
-        return null;
-    }
-
     Parser parseList() {
-        if (text == null && results == null)
+        if (text.isEmpty() && results == null)
             return this;
-        text.split("\\)");
 
-        return  null;
-    }
-    Parser AND(Parser p) {
-        return null;
+        return parserSymbols("^\\(", (str) -> new Var(str)).
+                parseSubList((res) -> {
+                    AList aList = new AList(new ArrayList<>());
+                    if (res == null)
+                        return aList;
+                    aList.list.add(res);
+                    return aList;
+                }).parserSymbols("^\\)", (str) -> results);
     }
 
     Parser OR(Parser p, Function<Parser, Parser> f) {
@@ -160,30 +174,179 @@ class Parser {
         return this;
     }
 
+    Parser parseSub() {
+        return parseList().OR(this, (p) ->
+                        p.parserSymbols("^#t", (str) ->
+                                new SBool(1))).
+                OR(this, (p) ->
+                        p.parserSymbols("^#f", (str) ->
+                                new SBool(0))).
+                OR(this,(p) ->
+                        p.parserSymbols("^[\\d]+", (str) ->
+                                new SInt(Long.parseLong(str)))).
+                OR(this, (p) -> p.parserSymbols("^[a-zA-Z][a-zA-Z0-9_@#!]*", (str) ->
+                        new Var(str))).
+                OR(this, (p) -> p.parserSymbols("^[-+*]", (str) ->
+                        new Var(str)));
+    }
+
     public AST parse(String text) {
         if (text.isEmpty() || (text == null))
             throw new RuntimeException("源代码不能为 null");
-        text = text.trim();
+        this.text = text.trim();
 
-        Parser parser = parseList().OR(this, (p) ->
-                        p.parserSymbols("#t", (str) ->
-                                new SBool(1))).
-                OR(this, (p) ->
-                        p.parserSymbols("#f", (str) ->
-                                new SBool(0))).
-                OR(this,(p) ->
-                        p.parserSymbols("^(\\d+)", (str) ->
-                                new SInt(Integer.parseInt(str)))).
-                OR(this, (p) -> p.parserSymbols("[a-zA-Z][a-zA-Z0-9_@#]", (str) ->
-                                new Var(str)));
+        Parser parser = parseSub();
 
         if (parser.results == null)
-            throw new RuntimeException("解析错误");
+            throw new RuntimeException("解析错误" + this.text);
 
         return new Program(parser.results);
     }
 }
 
+interface PassBase<IR> {
+    public IR run(IR ir);
+}
+
+class PassManager<T, P extends PassBase<T>> implements PassBase<T> {
+    ArrayList<P> list;
+
+    @Override
+    public T run(T t) {
+        for (P pass : list) {
+            t = pass.run(t);
+        }
+
+        return t;
+    }
+
+    PassManager() {
+        list = new ArrayList<>();
+    }
+    public void addPass(P pass) {
+        list.add(pass);
+    }
+}
+
+class VerifierScheme implements PassBase<AST> {
+    class Default implements ASTVisitor {
+        @Override
+        public <T> T forVar(Var v) {
+            throw new RuntimeException("语法错误：" +  v.toString());
+        }
+        @Override
+        public <T> T forSBool(SBool sBool) {
+            throw new RuntimeException("语法错误：" +  sBool.toString());
+
+        }
+        @Override
+        public <T> T forSInt(SInt sInt) {
+            throw new RuntimeException("语法错误：" +  sInt.toString());
+        }
+        @Override
+        public <T> T forAList(AList aList) {
+            throw new RuntimeException("语法错误：" +  aList.toString());
+        }
+        @Override
+        public <T> T forProgram(Program program) {
+            throw new RuntimeException("语法错误：" +  program.toString());
+        }
+    }
+
+    class StatementThird extends Default {
+        String var;
+        ArrayList<String> vars;
+        ArrayList<String> binop = new ArrayList<>(Arrays.asList(new String[]{"+", "-", "*"}));
+        StatementThird(ArrayList<String> vars, String var) {
+            this.vars = vars;
+            this.var = var;
+        }
+        @Override
+        public Var forVar(Var v) {
+            vars.contains(v.toString());
+            return v;
+        }
+
+        @Override
+        public SInt forSInt(SInt sInt) {
+            return sInt;
+        }
+
+        @Override
+        public AList forAList(AList aList) {
+            if (aList.list.size() != 3)
+                throw new RuntimeException("语法错误：" + aList.toString());
+            if(binop.contains(aList.list.get(0).toString()) &&
+                    aList.list.get(1).toString().equals(var)) {
+                AST ast = aList.list.get(2);
+                if (ast instanceof SInt) {
+                    if ((-Math.pow(2, 31)) < ((SInt) ast).i && ((SInt) ast).i < Math.pow(2, 31))
+                        return aList;
+                    else
+                        throw new RuntimeException("语法错误：" + aList.toString());
+                } else if (ast instanceof Var) {
+                    if (vars.contains(ast.toString()))
+                        return aList;
+                    else
+                        throw new RuntimeException("语法错误：" + aList.toString());
+                } else
+                    throw new RuntimeException("语法错误：" + aList.toString());
+            } else
+                throw new RuntimeException("语法错误：" + aList.toString());
+        }
+    }
+
+    class Statement extends Default {
+        ArrayList<String> vars = new ArrayList<>(Arrays.asList(new String[]{"rax", "rcx", "rdx", "rbx", "rbp", "rsi", "rdi",
+                "r8", "r9", "r10", "r11", "r12", "r13", "r14", "r15"}));
+        @Override
+        public AList forAList(AList aList) {
+            if (aList.list.size() != 3)
+                throw new RuntimeException("语法错误：" + aList.toString());
+            if (aList.list.get(0).toString().equals("set!") &&
+                    vars.contains(aList.list.get(1).toString())) {
+                aList.list.get(2).accept(new StatementThird(vars, aList.list.get(1).toString()));
+            } else
+                throw new RuntimeException("语法错误：" + aList.toString());
+            return aList;
+        }
+    }
+
+    class Begin extends Default {
+        @Override
+        public AList forAList(AList aList) {
+            Iterator<AST> it =  aList.list.iterator();
+            if (!it.hasNext() || !it.next().toString().equals("begin"))
+                throw new RuntimeException("语法错误：" + aList.toString());
+            for (;;) {
+                AST ast = it.next();
+                ast.accept(new Statement());
+                if (!it.hasNext()) {
+                    if(!((AList)ast).list.get(1).toString().equals("rax"))
+                        throw new RuntimeException("语法错误：" + aList.toString());
+                    return aList;
+                }
+            }
+        }
+
+        @Override
+        public Program forProgram(Program program) {
+            program.program.accept(this);
+            return program;
+        }
+    }
+    @Override
+    public AST run(AST ast) {
+        return ast.accept(new Begin());
+    }
+}
+class GenerateX8664 implements PassBase<AST> {
+
+    @Override
+    public AST run(AST ast) {
+        return null;
+    }
+}
 public class Assignment1 {
     public static String test(String str, String pattern) {
         Pattern p = Pattern.compile(pattern);
@@ -194,38 +357,26 @@ public class Assignment1 {
         System.out.println(s + " : " + m.replaceFirst(""));
 
         return s;
-
     }
     public static void main(String[] args) {
-        String str = "(begin\n" +
-                "(set! rax 8)\n" +
-                "(set! rcx 3)\n" +
-                "(set! rax (- rax rcx)))";
+        String str = " ( begin\n" +
+                "(  set! rax 5000000000)\n" +
+                "( set! rcx 3)\n" +
+                "( set! rax (- rax rcx)))";
 
-//        String s1 = "   sadd";
-//        String s2 = "s  add";
-//        String s3 = "sadd   ";
-//
-//        String[] strs1 = s1.split("^[\\s\\t\\n\\r]+", 2);
-//        System.out.println(Arrays.toString(strs1));
-//        String[] strs2 = s2.split("^[\\s\\t\\n\\r]+", 2);
-//        System.out.println(Arrays.toString(strs2));
-//        String[] strs = s3.split("^[\\s\\t\\n\\r]+", 2);
-//        System.out.println(Arrays.toString(strs));
+        Parser p = new Parser();
+        AST ast = p.parse(str);
 
-        String str4 = "#t#tsadasd";
-        Pattern p = Pattern.compile("#t");
-        Matcher m = p.matcher(str4);
-        m.find();
-        System.out.println(m.group() + " : " + m.replaceFirst(""));
+        VerifierScheme verifierScheme = new VerifierScheme();
 
-//        test("00123dada", "^(\\d+)");
+        AST ast1 = verifierScheme.run(ast);
 
-        System.out.println(Integer.parseInt(test("00123 dada", "^(\\d+)")));
+        String s = ast1.toString();
 
-//        Parser p = new Parser();
-//        p.parse(str);
+        System.out.println(s);
 
+//        Pattern pattern = Pattern.compile("^[-+*]");
+//        Matcher m = pattern.matcher("/ 1 2");
 
     }
 }
